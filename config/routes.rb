4daf5483e0a96b941ea8f1415b739dc8cc60665a
Yoosuf <mayoosuf@gmail.com>
2019-08-16 20:55:27 +0530

    ssl version

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'about', to: 'home#about'
+  get 'contact', to: 'home#contact'
 end
